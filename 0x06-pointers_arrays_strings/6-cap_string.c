5f20260ca4004fdd222c48864577517b3a6eec22
OmarKhalil10 <omar.khalil498@gmail.com>
2022-09-22 08:45:56 -0700

    initial commit

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,58 +1,112 @@
 #include "main.h"
 
+
 /**
 
- * cap_string - capitalizes all words of a string
+ * separator - checks if character is a seperator
 
- * @s: input string.
+ * @c: character to be checked
 
- * Return: the pointer to dest.
+ * Return: if seperator return 1. Otherwise return 0;
 
  */
 
 
+int separator(char c)
+
+{
+
+	/*Delcaring condition switch*/
+
+	switch (c)
+
+	{
+
+	case ' ':
+
+	case '\t':
+
+	case '\n':
+
+	case ',':
+
+	case ';':
+
+	case '.':
+
+	case '!':
+
+	case '?':
+
+	case '"':
+
+	case '(':
+
+	case ')':
+
+	case '{':
+
+	case '}':
+
+		return (1);
+
+
+	default:
+
+		return (0);
+
+	}
+
+
+}
+
+/**
+
+ * cap_string - capitalizes chars after given deliminators
+
+ * @s: string to uppercase
+
+ * Return: returns modified string
+
+ */
 
 char *cap_string(char *s)
 
 {
 
-int count = 0, i;
-
-int separators[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int count, upper;
 
 
-if (*(s + count) >= 97 && *(s + count) <= 122)
+	upper = -32; /*value constant 32*/
 
-*(s + count) = *(s + count) - 32;
 
-count++;
+	count = 0;
 
-while (*(s + count) != '\0')
+	/*Start WHILE*/
 
-{
+	while (s[count] != '\0')
 
-for (i = 0; i < 13; i++)
+	{
 
-{
+		/*letters lowercase*/
 
-if (*(s + count) == separators[i])
+		if (s[count] >= 'a' && s[count] <= 'z')
 
-{
+		{
 
-if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+			/*Convert uppercase*/
 
-*(s + (count + 1)) = *(s + (count + 1)) - 32;
+			if (s[count] == *s || separator(s[count - 1]))
 
-break;
 
-}
+				s[count] += upper;
 
-}
+		}
 
-count++;
+		count++; /*Add count*/
 
-}
+	}
 
-return (s);
+	return (s);
 
 }
