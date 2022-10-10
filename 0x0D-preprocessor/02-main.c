ea8c55b0cf5df4f0f5eef70fb056be69c57914d3
Omar Khalil <omarkhalil770@gmail.com>
2022-10-11 00:14:23 +0200

    initial commit

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -5,7 +5,7 @@
  * compiled from, followed by a new line.
  * Return: Always 0 (Success)
  */
-int main(void)
+int main ()
 {
 	printf("%s\n", __FILE__);
 	return (0);
---
+++ 0x0D-preprocessor/101-preprocessor_abuse.c
@@ -0,0 +1,7 @@
+/**
+ * main - program that can print Hello, Holberton, followed by a new line.
+ */
+void main(void)
+{
+	write(0, "Hello, Holberton\n", 17);
+}
