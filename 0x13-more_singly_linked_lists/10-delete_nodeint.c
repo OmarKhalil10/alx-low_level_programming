2f16fac0d47e4cd0d4ddab6b42f1a3824d4d75b5
Omar Khalil <omarkhalil770@gmail.com>
2022-10-26 04:46:31 +0200

    initial commit

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,47 +1,58 @@
 #include "lists.h"
+
 /**
- * delete_nodeint_at_index - function that deletes the
- * node at index index of a listint_t linked list.
- * @head: input
- * @index: input
- * Return: 1 if it succeeded, -1 if it failed
+ * delete_nodeint_at_index - deletes a node at an index
+ * @head: pointer to the head of the list
+ * @index: index of the node to be added
+ *
+ * Return: the address of the node
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	unsigned int i = NULL;
-	listint_t *del = NULL;
-	listint_t *tmp = NULL;
-	listint_t *current = NULL;
+	listint_t *old_node = NULL;
+	listint_t *previous_node = NULL;
+	unsigned int i = 0, list_len = listint_len(*head);
 
-	if (head != NULL && (*head) != NULL)
+	if ((index > list_len) || (list_len == 0))
+		return (-1);
+	while (head != NULL)
 	{
-		i = 1;
-		current = *head;
-		while (i < index)
+		if (i == index)
 		{
-			tmp = current->next;
-			if (tmp != NULL)
+			old_node = *head;
+			if (i == 0)
 			{
-				current = tmp;
+				*head = old_node->next;
+				free(old_node);
+				return (1);
 			}
-			else
-			{
-				return (-1);
-			}
-			i++;
+			previous_node->next = old_node->next;
+			free(old_node);
+			return (1);
 		}
-		if (index == 0)
-		{
-			del = current;
-			*head = current->next;
-		}
-		else
-		{
-			del = current->next;
-		}
-		current->next = del->next;
-		free(del);
-		return (1);
+		else if ((i + 1) == index)
+			previous_node = *head;
+		head = &((*head)->next);
+		i++;
 	}
 	return (-1);
+}
+
+/**
+ * listint_len - counts the number of nodes in a linked list
+ * @h: head of the list
+ *
+ * Return: the number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *cursor = h;
+	size_t count = 0;
+
+	while (cursor != NULL)
+	{
+		count += 1;
+		cursor = cursor->next;
+	}
+	return (count);
 }
